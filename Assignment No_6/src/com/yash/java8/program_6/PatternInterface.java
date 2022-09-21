5e0a5ad369d8c34b6361e12fcd43badd86505328
ajay.khandekar <ajay.khandekar@YPH1016100LT.Yash.com>
2022-09-21 17:49:24 +0530

    Java_8 Assignment

--- Assignment No_6/src/com/yash/java8/program_6/PatternInterface.java
+++
@@ -1,7 +0,0 @@
-package com.yash.java8.program_6;
-
-public interface PatternInterface {
-	
-	 void displayPattern(int row);
-	
-}
--- Assignment No_6/src/com/yash/java8/program_7/Calculate.java
+++
@@ -1,8 +0,0 @@
-package com.yash.java8.program_7;
-
-public interface Calculate {
-	
-	 public double calculate(double principal, double rate, double time, double emi);
-	
-
-}
